2d7b58e87650a9f75891eca010080d94a6264bf7
Zoltan <znalbert@gmail.com>
2016-05-19 20:42:56 -0500

    fix: Menu specs more robust; README.md edits
    The menu specs now test for hasClass instead of checking that the class attr
    is equal to 'menu-hidden'.
    
    Updated the wording on the README to reflect my past tense actions taken
    to complete this project.
    
    Also, added in better comments to my feedreader.js file.

--- README.md
+++ README.md
@@ -15,19 +15,19 @@ This project has a focus on learning the ins, outs, and importance of test-drive
 First, I learned that testing really isn't that difficult a process for both synchronous and asynchronous functions. More importantly, though, I learned that thinking about tests first really helps to understand one's project before even getting started and helps to ensure that new features and functionality work with the pre-existing ones.
 
 
-## Steps used to complete this project?
+## Steps used to complete this project
 
-1. Download the [required project assets](http://github.com/udacity/frontend-nanodegree-feedreader).
-2. Review the functionality of the application within your browser.
-3. Explore the application's HTML (*./index.html*), CSS (*./css/style.css*) and JavaScript (*./js/app.js*) to gain an understanding of how it works.
-4. Explore the Jasmine spec file in *./jasmine/spec/feedreader.js*
-5. Edit the allFeeds variable in *./js/app.js* to make the provided test fail and see how Jasmine visualizes this failure in your application.
-6. Return the allFeeds variable to a passing state.
-7. Write a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
-8. Write a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.
-9. Write a new test suite named "The menu".
-10. Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
-11. Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
-12. Write a test that ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container. Remember, loadFeed() is asynchronous so this test will require the use of Jasmine's beforeEach and asynchronous done() function.
-13. Write a test that ensures when a new feed is loaded by the loadFeed function that the content actually changes. Remember, loadFeed() is asynchronous.
-14. When complete - all of your tests should pass.
+1. Downloaded the [required project assets](http://github.com/udacity/frontend-nanodegree-feedreader).
+2. Reviewed the functionality of the application in the browser.
+3. Explored the application's HTML (*./index.html*), CSS (*./css/style.css*) and JavaScript (*./js/app.js*) to gain an understanding of how it works.
+4. Explored the Jasmine spec file in *./jasmine/spec/feedreader.js*
+5. Edited the allFeeds variable in *./js/app.js* to make the provided test fail and see how Jasmine visualizes this failure in your application.
+6. Returned the allFeeds variable to a passing state.
+7. Wrote a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
+8. Wrote a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.
+9. Wrote a new test suite named "The menu".
+10. Wrote a test that ensures the menu element is hidden by default. Analyzed the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
+11. Wrote a test that ensures the menu changes visibility when the menu icon is clicked. This test has two expectations: does the menu display when clicked and does it hide when clicked again.
+12. Wrote a test that ensures when the asynchronous loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container.
+13. Wrote a test that ensures when a new feed is loaded by the loadFeed function that the asynchronous content actually changes.
+14. All of your tests are now passing.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,85 +14,104 @@ $(function() {
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
 
-         it('feeds have URLs', function() {
-           allFeeds.forEach(function(feed) {
-             expect(feed.url).toBeDefined();
-             expect(feed.url.length).not.toBe(0);
-           })
-         });
+      /* Checks to make sure that allFeeds is defined and has a
+       * length greater than 0.
+       */
+      it('are defined', function() {
+          expect(allFeeds).toBeDefined();
+          expect(allFeeds.length).not.toBe(0);
+      });
 
-         it('feeds have names', function() {
-           allFeeds.forEach(function(feed) {
-             expect(feed.name).toBeDefined();
-             expect(feed.name.length).not.toBe(0);
-           })
-         });
+      /* Checks to make sure that each of the feeds has a url
+       * that is defined and has a length greater than 0.
+       */
+      it('feeds have URLs', function() {
+        allFeeds.forEach(function(feed) {
+          expect(feed.url).toBeDefined();
+          expect(feed.url.length).not.toBe(0);
+        })
+      });
+
+      /* Checks to make sure that each of the feeds has a name
+       * that is defined and has a length greater than 0.
+       */
+      it('feeds have names', function() {
+        allFeeds.forEach(function(feed) {
+          expect(feed.name).toBeDefined();
+          expect(feed.name.length).not.toBe(0);
+        })
+      });
     });
 
 
     /* This portion tests the menu's functionality */
     describe('The menu', function() {
-       var body;
+      var body;
 
-       beforeEach(function(){
-         body = $('body');
-       })
+      beforeEach(function(){
+        body = $('body');
+      })
 
-       it('menu should be hidden by default', function(){
-         expect(body.attr('class')).toEqual('menu-hidden');
-       });
+      /* Checks that the menu starts out hidden */
+      it('menu should be hidden by default', function(){
+        expect(body.hasClass('menu-hidden')).toEqual(true);
+      });
 
-       it('should display/hide appropriately on click', function(){
-         var menu = $('.menu-icon-link');
+      /* Checks that the menu displays/hides as appropriate
+       * when clicked
+       */
+      it('should display/hide appropriately on click', function(){
+        var menu = $('.menu-icon-link');
 
-         menu.click();
-         expect(body.attr('class')).not.toEqual('menu-hidden');
-         menu.click();
-         expect(body.attr('class')).toEqual('menu-hidden');
-       });
+        menu.click();
+        expect(body.hasClass('menu-hidden')).toEqual(false);
+        menu.click();
+        expect(body.hasClass('menu-hidden')).toEqual(true);
+      });
 
     });
 
 
     /* Test to ensure feeds get loaded when the site loads */
     describe('Initial Entries', function() {
+      /* Load the asynchronous data */
       beforeEach(function(done) {
-        loadFeed(0, function() {
-          done();
-        });
+        loadFeed(0, done);
       });
 
-      it('feed container should have at least 1 entry', function(done) {
+      /* Makes sure that at least one entry is loaded into the
+       * feeds container.
+       */
+      it('feed container should have at least 1 entry', function() {
         var entries = $('.feed').find('.entry');
         expect(entries.length).toBeGreaterThan(0);
-        done();
       });
     })
 
 
     /* Tests to ensure that changing the feed works */
     describe('New Feed Selection', function() {
-      var firstFeedContents;
-      var secondFeedContents;
+      var firstFeedContents, secondFeedContents;
 
+      /* Loads and stores the data from two separate calls to
+       * loadFeeds.
+       */
       beforeEach(function(done) {
         loadFeed(1, function() {
           firstFeedContents = $('.entry')[0].innerHTML;
-          done();
-        });
+          loadFeed(0, function() {
+            secondFeedContents = $('.entry')[0].innerHTML;
+            done();
+          })
+        })
       });
 
+      /* Checks that the two calls to loadFeeds actually have
+       * loaded different data.
+       */
       it('loadFeed updates feed content displayed', function(done) {
-        loadFeed(0, function(){
-          secondFeedContents = $('.entry')[0].innerHTML;
-          expect(firstFeedContents).not.toEqual(secondFeedContents);
-          done();
-        })
+        expect(firstFeedContents).not.toEqual(secondFeedContents);
       });
     })
 
